fd9540dfa9e347b6341e1b687a06edc41d7ab9f2
Deip Kumar <deip@pano.co>
2024-01-11 16:44:54 -0800

    Reserve PyPI package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gradial',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/Gradial/gradial',
+    author='Deip Kumar',
+    author_email='deip@pano.co',
+    packages=['gradial'],
+    classifiers=['Development Status :: 1 - Planning'],
+    zip_safe=False
+)
